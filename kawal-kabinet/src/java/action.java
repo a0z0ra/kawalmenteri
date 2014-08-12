c06a3a30257ae2fc9e1a58228a3a04a9daddd5b4
khairulanshar <khairulanshar@khairulanshar.local>
2014-08-12 08:05:03 +0800

    khairulUpdate

--- kawal-kabinet/src/java/action.java
+++ kawal-kabinet/src/java/action.java
@@ -23,6 +23,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.DecimalFormat;
 import java.util.Date;
 import java.util.LinkedHashMap;
@@ -70,9 +72,241 @@ public class action extends HttpServlet {
                 }
                 Object obj = JSONValue.parse(sb.toString());
                 JSONObject obj1 = (JSONObject) obj;
+                String admin = "N";
+                String email = obj1.get("email").toString();
+                String first_name = obj1.get("first_name").toString();
+                String gender = obj1.get("gender").toString();
+                String id = obj1.get("id").toString();
+                String last_name = obj1.get("last_name").toString();
+                String link = obj1.get("link").toString();
+                String name = obj1.get("name").toString();
+                String verified = obj1.get("verified").toString();
+
+                DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+                Key linkKey = KeyFactory.createKey("userTable", "user");
+                // Run an ancestor query to ensure we see the most up-to-date
+                // view of the Greetings belonging to the selected Guestbook.
+
+                Filter posisinama = new FilterPredicate("link", FilterOperator.EQUAL, link.toLowerCase());
+                // Run an ancestor query to ensure we see the most up-to-date
+                // view of the Greetings belonging to the selected Guestbook.
+                Query query = new Query("userTable", linkKey).setFilter(posisinama);
+                List<Entity> userTables = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(1));
+                Date date = new Date();
+
+                if (userTables.isEmpty()) {
+                    Entity userTable = new Entity("userTable", linkKey);
+                    userTable.setProperty("email", email);
+                    userTable.setProperty("first_name", first_name);
+                    userTable.setProperty("gender", gender);
+                    userTable.setProperty("id", id);
+                    userTable.setProperty("last_name", last_name);
+                    userTable.setProperty("link", link.toLowerCase());
+                    userTable.setProperty("name", name);
+                    userTable.setProperty("verified", verified);
+                    userTable.setProperty("lastLogin", date);
+                    if (email.equalsIgnoreCase("khairul.anshar@gmail.com")
+                            || id.equalsIgnoreCase("112525777678499279265")
+                            || id.equalsIgnoreCase("10152397276159760")
+                            || name.equalsIgnoreCase("Khairul Anshar")) {
+                        userTable.setProperty("admin", "Y");
+                    } else {
+                        userTable.setProperty("admin", admin);
+                    }
+                    datastore.put(userTable);
+                } else {
+                    for (Entity userTable : userTables) {
+                        admin = userTable.getProperty("admin").toString();
+                        userTable.setProperty("lastLogin", date);
+                        datastore.put(userTable);
+                    }
+                }
+                if (email.equalsIgnoreCase("khairul.anshar@gmail.com")
+                        || id.equalsIgnoreCase("112525777678499279265")
+                        || id.equalsIgnoreCase("10152397276159760")
+                        || name.equalsIgnoreCase("Khairul Anshar")) {
+                    admin = "Y";
+                }
+                obj1.put("admin", admin);
                 session.setAttribute("userAccount", obj1);
                 record.put("userAccount", obj1);
+
             } catch (Exception e) {
+            }
+            response.setContentType("text/html;charset=UTF-8");
+            out.print(JSONValue.toJSONString(record));
+            out.flush();
+        }
+        if (form_action.equalsIgnoreCase("getiframeData")) {
+            StringBuffer sb = new StringBuffer();
+            String line = null;
+            LinkedHashMap record = new LinkedHashMap();
+            try {
+                BufferedReader reader = request.getReader();
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                Object obj = JSONValue.parse(sb.toString());
+                //JSONArray records = (JSONArray) obj;
+                JSONObject obj1 = (JSONObject) obj;
+                String src = obj1.get("src").toString();
+                
+                final URL url = new URL(src);
+                final URLConnection urlConnection = url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+                urlConnection.connect();
+                final InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader is = new InputStreamReader(inputStream);
+                StringBuilder sb1 = new StringBuilder();
+                BufferedReader br = new BufferedReader(is);
+                String read = br.readLine();
+                while (read != null) {
+                    sb1.append(read);
+                    read = br.readLine();
+                }
+                record.put("data", sb1.toString());
+               record.put("status", "OK");
+            } catch (Exception e) {
+                record.put("status", "error");
+                record.put("errormsg", e.toString());
+            }
+            response.setContentType("text/html;charset=UTF-8");
+            out.print(JSONValue.toJSONString(record));
+            out.flush();
+        }
+        if (form_action.equalsIgnoreCase("postCommentPosisi")) {
+            StringBuffer sb = new StringBuffer();
+            String line = null;
+            LinkedHashMap record = new LinkedHashMap();
+            try {
+                BufferedReader reader = request.getReader();
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                Object obj = JSONValue.parse(sb.toString());
+                //JSONArray records = (JSONArray) obj;
+                JSONObject obj1 = (JSONObject) obj;
+                JSONObject userAccount = (JSONObject) session.getAttribute("userAccount");
+                String dept = obj1.get("dept").toString();
+                String star = obj1.get("star").toString();
+                String comment = obj1.get("comment").toString();
+                String id = userAccount.get("id").toString();
+                String name = userAccount.get("name").toString();
+                String link = userAccount.get("link").toString();
+                postData(name, dept, "", star, comment, id, "AlasanStarPosisi", dept, link, link);
+                postData2(name, dept, "", star, comment, id, "AlasanStarCalonPosisi", "dept", dept, link);
+
+                record.put("status", "OK");
+            } catch (Exception e) {
+                record.put("status", "error");
+                record.put("errormsg", e.toString());
+            }
+            response.setContentType("text/html;charset=UTF-8");
+            out.print(JSONValue.toJSONString(record));
+            out.flush();
+        }
+        if (form_action.equalsIgnoreCase("getMyCommentPosisi")) {
+            StringBuffer sb = new StringBuffer();
+            String line = null;
+            LinkedHashMap record = new LinkedHashMap();
+            String dept = "";
+            try {
+                BufferedReader reader = request.getReader();
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                Object obj = JSONValue.parse(sb.toString());
+                //JSONArray records = (JSONArray) obj;
+                JSONObject obj1 = (JSONObject) obj;
+                dept = obj1.get("dept").toString();
+                JSONObject userAccount = (JSONObject) session.getAttribute("userAccount");
+                String id_ = userAccount.get("id").toString();
+                String link_ = userAccount.get("link").toString();
+                DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+                Key guestbookKey = KeyFactory.createKey(dept, link_);
+                // Run an ancestor query to ensure we see the most up-to-date
+                // view of the Greetings belonging to the selected Guestbook.
+                Query query = new Query("AlasanStarPosisi", guestbookKey).addSort("date", Query.SortDirection.DESCENDING);
+                //List<Entity> AlasanStars = datastore.prepare(query);
+                PreparedQuery pq = datastore.prepare(query);
+                LinkedHashMap record1 = new LinkedHashMap();
+                for (Entity AlasanStar : pq.asIterable()) {
+                    String id = AlasanStar.getProperty("user").toString();
+                    String date = AlasanStar.getProperty("date").toString();
+                    String star = AlasanStar.getProperty("star").toString();
+                    String comment = AlasanStar.getProperty("comment").toString();
+                    String name = AlasanStar.getProperty("name").toString();
+                    String link = AlasanStar.getProperty("link").toString();
+                    record1.put("id", id);
+                    record1.put("date", date);
+                    record1.put("star", star);
+                    record1.put("comment", comment);
+                    record1.put("name", name);
+                    record1.put("link", link);
+                }
+                record.put("AlasanStar", record1);
+
+                guestbookKey = KeyFactory.createKey("dept", dept);
+                // Run an ancestor query to ensure we see the most up-to-date
+                // view of the Greetings belonging to the selected Guestbook.
+                query = new Query("AlasanStarCalonPosisi", guestbookKey).addSort("date", Query.SortDirection.DESCENDING);
+                //List<Entity> AlasanStars = datastore.prepare(query);
+                pq = datastore.prepare(query);
+                JSONArray obj11 = new JSONArray();
+                JSONArray obj11p = new JSONArray();
+                JSONArray obj11n = new JSONArray();
+                int i = 0;
+                int ip = 0;
+                int in = 0;
+                double total = 0;
+                double totalp = 0;
+                double totaln = 0;
+                for (Entity AlasanStar : pq.asIterable()) {
+                    record1 = new LinkedHashMap();
+                    String id = AlasanStar.getProperty("user").toString();
+                    String date = AlasanStar.getProperty("date").toString();
+                    String star = AlasanStar.getProperty("star").toString();
+                    String comment = AlasanStar.getProperty("comment").toString();
+                    comment = comment.replaceAll("\n", "<br/>");
+                    String name = AlasanStar.getProperty("name").toString();
+                    String link = AlasanStar.getProperty("link").toString();
+                    record1.put("id", id);
+                    record1.put("date", date);
+                    record1.put("star", star);
+                    record1.put("comment", comment);
+                    record1.put("name", name);
+                    record1.put("link", link);
+                    obj11.add(record1);
+                    i++;
+                    double d = Double.parseDouble(star);
+                    total = total + d;
+                    if (d >= 0) {
+                        obj11p.add(record1);
+                        ip++;
+                        totalp = totalp + d;
+                    } else {
+                        obj11n.add(record1);
+                        in++;
+                        totaln = totaln + d;
+                    }
+                }
+                double avg = total / i;
+                if (i == 0) {
+                    avg = 0;
+                }
+                DecimalFormat df = new DecimalFormat("#.##");
+                record.put("total", total);
+                record.put("totalp", totalp);
+                record.put("totaln", totaln);
+                record.put("avg", df.format(avg));
+                //record.put("AlasanStars", obj11);
+                record.put("AlasanStarsp", obj11p);
+                record.put("AlasanStarsn", obj11n);
+                record.put("status", "OK");
+            } catch (Exception e) {
+                record.put("status", "error");
+                record.put("errormsg", e.toString());
             }
             response.setContentType("text/html;charset=UTF-8");
             out.print(JSONValue.toJSONString(record));
@@ -98,7 +332,7 @@ public class action extends HttpServlet {
                 String id = userAccount.get("id").toString();
                 String name = userAccount.get("name").toString();
                 String link = userAccount.get("link").toString();
-                postData(name, dept, namaCalon, star, comment, id, "AlasanStar", dept + namaCalon, id, link);
+                postData(name, dept, namaCalon, star, comment, id, "AlasanStar", dept + namaCalon, link, link);
                 postData2(name, dept, namaCalon, star, comment, id, "AlasanStarCalon", dept, namaCalon, link);
 
                 record.put("status", "OK");
@@ -126,10 +360,11 @@ public class action extends HttpServlet {
                 JSONObject obj1 = (JSONObject) obj;
                 JSONObject userAccount = (JSONObject) session.getAttribute("userAccount");
                 String id_ = userAccount.get("id").toString();
+                String link_ = userAccount.get("link").toString();
                 dept = obj1.get("dept").toString();
                 namaCalon = obj1.get("namaCalon").toString();
                 DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-                Key guestbookKey = KeyFactory.createKey(dept + namaCalon, id_);
+                Key guestbookKey = KeyFactory.createKey(dept + namaCalon, link_);
                 // Run an ancestor query to ensure we see the most up-to-date
                 // view of the Greetings belonging to the selected Guestbook.
                 Query query = new Query("AlasanStar", guestbookKey).addSort("date", Query.SortDirection.DESCENDING);
@@ -436,6 +671,7 @@ public class action extends HttpServlet {
                     String nama = userAccount.get("name").toString();
                     String email = userAccount.get("email").toString();
                     String link = userAccount.get("link").toString();
+                    String admin = userAccount.get("admin").toString();
                     Date date = new Date();
                     Entity psosisiEntity = new Entity("posisi", typeKey);
                     psosisiEntity.setProperty("user", id);
@@ -448,8 +684,11 @@ public class action extends HttpServlet {
                     if (email.equalsIgnoreCase("khairul.anshar@gmail.com")
                             || id.equalsIgnoreCase("112525777678499279265")
                             || id.equalsIgnoreCase("10152397276159760")
-                            || nama.equalsIgnoreCase("Khairul Anshar")) {
+                            || nama.equalsIgnoreCase("Khairul Anshar")
+                            || admin.equalsIgnoreCase("Y")) {
                         psosisiEntity.setProperty("reviewed", "Y");
+                        psosisiEntity.setProperty("nama", "Kawal Menteri");
+                        psosisiEntity.setProperty("link", "https://www.facebook.com/KawalMenteri");
                     } else {
                         psosisiEntity.setProperty("reviewed", "N");
                     }
@@ -635,6 +874,7 @@ public class action extends HttpServlet {
                     String nama = userAccount.get("name").toString();
                     String email = userAccount.get("email").toString();
                     String link = userAccount.get("link").toString();
+                    String admin = userAccount.get("admin").toString();
                     Date date = new Date();
                     Entity psosisiEntity = new Entity("kandidat", typeKey);
                     psosisiEntity.setProperty("user", id);
@@ -649,8 +889,11 @@ public class action extends HttpServlet {
                     if (email.equalsIgnoreCase("khairul.anshar@gmail.com")
                             || id.equalsIgnoreCase("112525777678499279265")
                             || id.equalsIgnoreCase("10152397276159760")
-                            || nama.equalsIgnoreCase("Khairul Anshar")) {
+                            || nama.equalsIgnoreCase("Khairul Anshar")
+                            || admin.equalsIgnoreCase("Y")) {
                         psosisiEntity.setProperty("reviewed", "Y");
+                        psosisiEntity.setProperty("nama", "Kawal Menteri");
+                        psosisiEntity.setProperty("link", "https://www.facebook.com/KawalMenteri");
                     } else {
                         psosisiEntity.setProperty("reviewed", "N");
                     }
@@ -732,6 +975,7 @@ public class action extends HttpServlet {
             AlasanStar.setProperty("star", star);
             AlasanStar.setProperty("comment", comment);
             AlasanStar.setProperty("name", name);
+            AlasanStar.setProperty("namaCalon", namaCalon);
             AlasanStar.setProperty("link", link);
             datastore.put(AlasanStar);
         } else {
@@ -742,6 +986,7 @@ public class action extends HttpServlet {
                 AlasanStar.setProperty("star", star);
                 AlasanStar.setProperty("comment", comment);
                 AlasanStar.setProperty("name", name);
+                AlasanStar.setProperty("namaCalon", namaCalon);
                 AlasanStar.setProperty("link", link);
                 datastore.put(AlasanStar);
             }
@@ -761,6 +1006,7 @@ public class action extends HttpServlet {
         AlasanStar.setProperty("star", star);
         AlasanStar.setProperty("comment", comment);
         AlasanStar.setProperty("name", name);
+        AlasanStar.setProperty("namaCalon", namaCalon);
         AlasanStar.setProperty("link", link);
         datastore.put(AlasanStar);
     }
